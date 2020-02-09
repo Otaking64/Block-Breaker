eb72178b228bddbc485a40f7fca7d2d08fb8e630
awestendorp <s1083928@student.hsleiden.nl>
2020-02-09 17:54:39 +0100

    unbreakable blocks and particles
    unbreakable blocks and particles

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,27 +4,61 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
-
+    [SerializeField] int maxHits;
     [SerializeField] AudioClip breakSound;
+    [SerializeField] GameObject blockSparklesVFX;
+
 
     Level level;
     GameStatus gameStatus;
+
+
+    [SerializeField] int timesHit;
+
+
     private void Start()
     {
-        level = FindObjectOfType<Level>();
-        level.CountBreakableBlocks();
-        gameStatus = FindObjectOfType<GameStatus>();
-        
+        CountBreakableBlocks();
+
     }
 
+    private void CountBreakableBlocks()
+    {
+        level = FindObjectOfType<Level>();
+        if (tag == "Breakable")
+        {
+            level.CountBlocks();
+        }
+        gameStatus = FindObjectOfType<GameStatus>();
+    }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
-        Destroy(gameObject, 0.2f);
-        level.BlockDestroyed();
-        gameStatus.AddToScore();
+        if(tag == "Breakable")
+        {
+            HandleHit();
+        }
 
+    }
+
+    private void HandleHit()
+    {
+        timesHit++;
+        if (timesHit >= maxHits)
+        {
+            AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
+            TriggerSparklesVFX();
+            Destroy(gameObject, 0.2f);
+            level.BlockDestroyed();
+            gameStatus.AddToScore();
+        }
+    }
+
+    private void TriggerSparklesVFX()
+    {
+        GameObject sparkles = Instantiate(blockSparklesVFX, transform.position, transform.rotation);
+
+        Destroy(sparkles, 2f);
 
     }
 
